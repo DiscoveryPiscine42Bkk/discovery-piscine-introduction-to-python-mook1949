3222f7765991b626665648139498d13910772722
mook1949 <boonyanuch191049@gmail.com>
2025-06-18 15:42:08 +0000

    add play_with_array.py

--- cell05/ex01/play_with_array.py
+++ cell05/ex01/play_with_array.py
@@ -1,4 +1,4 @@
 Original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-New_array =  [4, 10, 11, 50, 10, 24, -10, 4]
 print("Original_array", Original_array)
+New_array =  {x + 2 for x in Original_array}
 print("New_array", New_array)
