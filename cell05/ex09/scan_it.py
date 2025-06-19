7e2141f92823ef526e979d146122c5208b5c1a63
mook1949 <boonyanuch191049@gmail.com>
2025-06-19 05:02:20 +0000

    add scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,12 @@
+import sys
+params = sys.argv[1:]
+if len(params) != 2:
+    print("none")
+else:
+    keyword = params
+    text = params[1]
+    count = text.count(keyword)
+    if count > 0:
+        print(count)
+    else:
+        print("none")
